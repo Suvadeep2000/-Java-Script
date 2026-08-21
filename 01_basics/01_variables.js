523184a701c611b448c35ae1b814642ec0078857
Suvadeep Giri <bubunsuvadeep@gmail.com>
2026-08-21 05:38:55 +0000

    conversion of data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "suvadeepgiri@gmail.com"
+var accoutPassword = "12345"
+accountCity = "Kalyani"
+
+//accountId = 2
+
+/*
+Never use "var", Use "let"
+*/
+console.log([accountId, accountEmail, accoutPassword]);
+
+console.table([accountId, accountEmail, accoutPassword]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict" // treat all JS code as newer version
+
+let name = "Suavdeep"
+let age = 26
+let isLoggerIn= false
+
+// number => 2 to power 53
+// bigint for big number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Bubun");
