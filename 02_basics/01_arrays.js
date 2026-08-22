9ea331a6532ec6f1df647b5ffb37db0bda987280
Suvadeep Giri <bubunsuvadeep@gmail.com>
2026-08-22 18:26:33 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+// Array
+
+const myArray = [0, 1, 2, 3, 4, 5]
+const myHeros = ["Spidy", "Iron Man"]
+
+const myArray2 = new Array(1, 2, 3, 4)
+console.log(myArray2[1]);
+
+// Array Methods
+
+// myArray.push(6)
+// console.log(myArray);
+// myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArray.includes(9));
+
+// const newArray = myArray.join()
+
+// console.log(myArray);
+// console.log(typeof(newArray));
+
+//Slice, Splice
+
