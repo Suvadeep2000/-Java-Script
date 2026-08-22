dc86dc295db23e19fd68925a6aef5dbe78b22406
Suvadeep Giri <bubunsuvadeep@gmail.com>
2026-08-22 18:27:14 +0000

    arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,10 @@
+const marvel_heros = ["Thor", "Ironman", "Spiderman"]
+const dc_heros = ["Superman", "Flash", "Batman"]
+
+// const AllHeros = marvel_heros.concat(dc_heros)
+// console.log(AllHeros);
+
+const All_new_heros = [...marvel_heros, ...dc_heros]
+
+// console.log(All_new_heros);
+
